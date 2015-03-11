0ce25b1b1a69213982d75f14443f789712a10ba0
Joshua Cox <joshua.n.cox@gmail.com>
2015-03-10 20:43:57 -0700

    room joining fixed
    -room joining checks for room before attempting to join
    -still need to refactor checking so makeRoom can use the same
    functionality
    -everything’s prettier

--- builds/development/js/controllers/room.js
+++ builds/development/js/controllers/room.js
@@ -2,7 +2,9 @@ myApp.controller('RoomController',
 	function($scope, $firebaseArray, $location, $routeParams, FIREBASE_URL) {
   
   $scope.room = {};
+  $scope.user = {};
   $scope.room.name = $routeParams.room;
+  $scope.user.name = 'Guest' + Math.floor(Math.random() * 10000);
 
 	var ref = new Firebase(FIREBASE_URL + '/rooms/' + $scope.room.name + '/logs');
   var logAry = $firebaseArray(ref);
@@ -14,11 +16,9 @@ myApp.controller('RoomController',
 	$scope.addLog = function() {
     logAry.$add({
       text: $scope.log.text,
+      user: $scope.user.name,
       date: Firebase.ServerValue.TIMESTAMP
     }).then(function() {
-      var chat = $('.chat-window');
-      chat.scrollTop(chat[0].scrollHeight);      
-
       $scope.log.text = '';
     });
   } // addlog
--- builds/development/js/controllers/welcome.js
+++ builds/development/js/controllers/welcome.js
@@ -1,8 +1,10 @@
 myApp.controller('WelcomeController', 
 	function($scope, $location, $firebaseArray, FIREBASE_URL) {
 
+  var ref = new Firebase(FIREBASE_URL + '/rooms');
+
 	$scope.makeRoom = function() { 
-    var roomName = $scope.room.name;
+    var roomName = $scope.room.make;    
     var roomRef = new Firebase(FIREBASE_URL + '/rooms/' + roomName);
     var roomAry = $firebaseArray(roomRef);    
 
@@ -11,12 +13,18 @@ myApp.controller('WelcomeController',
       date: Firebase.ServerValue.TIMESTAMP
     })
     .then(function(roomRef){
-      console.log('success');
       $location.path('/room/' + roomName);
     });
   } // add room
 
   $scope.joinRoom = function() {
-    // join room
-  }
+    ref.child($scope.room.join).once('value', function(snapshot) {
+      var exists = (snapshot.val() !== null);
+      if (exists) {
+        $scope.$apply($location.path('/room/' + $scope.room.join));
+      } else {
+        $scope.$apply($scope.message = 'sorry, no room found with that name');
+      }
+    });
+  } // join room
 });
